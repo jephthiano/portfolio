c43ebf9720c04383d4bffc8edb9d7a6d88ad44a5
OLADEJO JEPHTHAH <jephthahooh@gmail.com>
2024-10-23 11:49:20 +0100

    revamped

--- src/addons/component/about_me.inc.js
+++ src/addons/component/about_me.inc.js
@@ -12,8 +12,8 @@ function AboutMe(props) {
       <>
       <div id='about_me'title='about oladejo jephthah'className='trigger j-round-large'>
         <div className='j-xlarge j-bolder j-container'><b>ABOUT ME</b></div>
-        <br/>
-        <div className='j-row'>
+        {/* <br/> */}
+        {/* <div className='j-row'>
           <div className='j-col m4 j-padding'>
             <center>
               <Image />
@@ -41,7 +41,36 @@ function AboutMe(props) {
               </center>
             </div>
           </div>
-        </div>
+        </div> */}
+        <div className='j-large j-padding'style={{lineHeight:'30px'}}>
+              <div style={{marginBottom:'10px'}}>
+                I'm a Fullstack Software/ Web Application Developer with over 4 years of experience in developing modern, mobile first,
+                responsive, scalable and fast softwares and APIs.
+              </div>
+              <div style={{marginBottom:'10px'}}>
+                I am an individual with passion for learning and transforming creative ideas into digital solutions and dedicated to delivering high-quality results. My strength lies in creativity, innovation 
+                and problem-solving to deliver the best result for user experience.
+              </div>
+              <div style={{marginBottom:'10px'}}>
+              <div className="j-bolder">My expertise are in the following aspects.</div>
+                <div className="j-padding">
+                  <div><span className="fas fa-arrow-right" style={{marginRight:"10px"}}></span>Fullstack Development</div>
+                  <div><span className="fas fa-arrow-right" style={{marginRight:"10px"}}></span>Frontend Development</div>
+                  <div><span className="fas fa-arrow-right" style={{marginRight:"10px"}}></span>Backend Logics/APIs Development</div>
+                  <div><span className="fas fa-arrow-right" style={{marginRight:"10px"}}></span>Database Architecture</div>
+                </div>
+              </div>
+              <div style={{marginBottom:'10px'}}>
+                I would love to work with you on your next project.
+              </div>
+              <center>
+                <span className={thColor.bgColor2} style={{fontFamily:'Sofia,sans-serif',margin:'5px 10px 5px 0px'}}>
+                  <div className='j-round j-btn j-bolder j-padding'onClick={() => horNav('contact_me',$('#t_contact_me'))}>
+                    Contact Me
+                  </div>
+                </span>
+              </center>
+            </div>
       </div>
       </>
     );
--- src/addons/state/reducer.js
+++ src/addons/state/reducer.js
@@ -1,11 +1,11 @@
 import {createSlice} from "@reduxjs/toolkit"
 
 const initialThemeStateValue = {
-    bgColor1: 'j-color4', 
-    bgColor2: 'j-color3',
-    txtColor1: 'j-text-color7',
-    txtColor2: 'j-text-color6',
-    border: 'j-border-2 j-border-color6'
+    bgColor1: 'j-color3', 
+    bgColor2: 'j-color4',
+    txtColor1: 'j-text-color6',
+    txtColor2: 'j-text-color7',
+    border: 'j-border-1 j-border-color2'
 };
 
 const themeSlice = createSlice({
